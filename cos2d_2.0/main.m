676342c85d93969b317fb4ce99a8c84bc5c340a1
Swen Johannes <s.johannes@student.rug.nl>
2021-12-03 12:36:26 +0100

    Vectorized/matrix efficiency M * A in Vk1k2 computation.

--- cos2d_2.0/main.m
+++ cos2d_2.0/main.m
@@ -1,9 +1,9 @@
 %% Params
-clear 
+clear
 clc
 
 %% Parameters
-N = 100;
+N = 300;
 k = 0:(N-1);
 
 S0 = 100;
@@ -21,7 +21,7 @@ x0 = log(S0 / K);
 h = log(L / K);
 
 T = 1;
-Nobs = 6;
+Nobs = 2;
 dt = T/ Nobs;
 
 %Obtain a and b from heston cumulants
@@ -38,7 +38,7 @@ b2 = 1.5;
 %% COS 2D
 V(:, :, Nobs) = zeros(N);           %Create empty V dataframe
 V(:, :, Nobs) = V_T(h, a1, b1, a2, b2, K, N); %Store V(T_M) at M
- 
+
 
 phi_Ap = phi_A(k, k, kappa, rho, eta, theta, r, dt, a1, b1, a2, b2);
 phi_Am = phi_A(k, -k, kappa, rho, eta, theta, r, dt, a1, b1, a2, b2);
@@ -47,22 +47,17 @@ if Nobs > 1
     % %Obtain H positive and minus
     Hp = get_H(kappa, rho, eta, dt, 1, a1, b1, a2, b2, N);
     Hm = get_H(kappa, rho, eta, dt, -1, a1, b1, a2, b2, N);
-    
+
     Mp = get_M2(a1, h, a1, b1, N); %Obtain M
 
     for t = (Nobs - 1):-1:1
-         %Obtain W positive and minus
-         Wp = 0.5 * exp(-r * dt) * phi_Ap .* V(:, :, t + 1);
-         Wm = 0.5 * exp(-r * dt) * phi_Am .* V(:, :, t + 1);
-            
-        %Obtain A
-        A = get_A(Hp, Hm, Wp, Wm, N);
-    
-        for k1 = 1:N
-            for k2 = 1:N          
-                V(k1, k2, t) = real(weighted_sum(Mp(k1, :) .* A(:, k2)'));
-            end
-        end
+        Wp = 0.5 * exp(-r * dt) * phi_Ap .* V(:, :, t + 1); %W positive
+        Wm = 0.5 * exp(-r * dt) * phi_Am .* V(:, :, t + 1); %W minus
+
+        A = get_A(Hp, Hm, Wp, Wm, N);      %Obtain A
+        A_ = A .* [0.5 repelem(1, N -1)]'; %Weight first row by a half!
+        V(:, :, t) = real(Mp * A); %Compute the weighted sum in matrix form
+
     end
 end
 %Final step: compute V0
